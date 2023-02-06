05ded082404922b85f880029fd4fc8f7c5aad811
Marcos Anderson de Sousa <marcoscpd2000@gmail.com>
2023-02-06 10:47:27 -0300

    Order, OrderModel

---
+++ src/main/java/br/com/fiap/abctechapi/model/OrderLocation.java
@@ -0,0 +1,21 @@
+package br.com.fiap.abctechapi.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderLocation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private Double latitude;
+    private Double longitude;
+    private Date date;
+}
